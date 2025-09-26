5653ecdbc787c0d82504e7658e9a1c407c5beb8e
shakibh10~ <billalsikdar03@gmail.com~>
2025-09-26 22:34:43 +0600

    string type

---
+++ LDay5/stringstyle.py
@@ -0,0 +1,9 @@
+str1 = " This is one type of string "
+str2 = ' This is another type of string '
+str3 = ''' This is a multi-line string'''
+str4 = """ This is also a multi-line string"""
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
